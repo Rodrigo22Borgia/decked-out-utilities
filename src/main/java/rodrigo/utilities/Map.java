add8dd5c955b15f214faac5405fdd3addb820fa6
Rodrigo22Borgia <vojta.vesecky@seznam.cz>
2026-09-17 15:04:26 +0200

    More map fixes, additional commands, clank & hazard calc

--- src/main/java/rodrigo/utilities/Map.java
+++ src/main/java/rodrigo/utilities/Map.java
@@ -35,20 +35,19 @@ public class Map {
             {102, 101, 101, 101, 101, -126, -128, -128, -128, 101, -126, -128, -128, -128, 101, -126, -128, -128, -128, 101, -126, -126, -126, -126, 102},
             {102, 100, 100, 100, 100, -126, 101, 101, 101, 100, -126, 101, 101, 101, 100, -126, 101, 101, 101, 100, -126, -128, -128, -128, 101}};
     private static final byte[] cardIcon = {32, 33, 33, 32, 33, 33};
-    private static final byte[] recycleIcon  = {-123, -124, -124, -123};
-
-    private static final byte[] offsets     = {6,3,5}; //main, cards, cards vertical
     private static final byte[] emberPos    = {30,34};
     private static final byte[] treasurePos = {30,57};
     private static final byte[] hazardPos   = {30,80};
     private static final byte[] clankPos    = {30,103};
-    private static final byte[] recyclePos  = {52,121};
+    private static final byte[] recyclePos  = {20,116};
     private static final byte[] cardPos     = {62,115};
 
     private byte embers    = 0;
     private byte treasure  = 0;
-    private byte hazard    = 0;
-    private byte clank     = 0;
+    private byte hazardB   = 0;
+    private byte hazard   = 0;
+    private byte clankB    = 0;
+    private byte clank    = 0;
     private byte recycles  = 0;
     private byte cards     = 0;
 
@@ -59,18 +58,6 @@ public class Map {
     public Map(int mapId, ServerLevel level) {
         this.mapId = mapId;
         this.mapData = level.getMapData(new MapId(mapId));
-    }
-
-    public int getValue(CommandContext<CommandSourceStack> context) {
-        return switch (StringArgumentType.getString(context, "type")) {
-            case "embers" -> embers;
-            case "treasure" -> treasure;
-            case "hazard" -> hazard;
-            case "clank" -> clank;
-            case "recycles" -> recycles;
-            case "cards" -> cards;
-            default -> 0;
-        };
     }
 
     public int drawPixel(CommandContext<CommandSourceStack> context) {
@@ -101,7 +88,7 @@ public class Map {
         }
     }
 
-    public void fill(int x, int y,int width, int height, byte colour) {
+    public void fill(int x, int y, int width, int height, byte colour) {
         for (int i = x; i < x + width; i++) {
             for (int j = y; j < y + height; j++) {
                 mapData.setColor(i,j, colour);
@@ -161,70 +148,6 @@ public class Map {
         scheduler.shutdown();
     }
 
-    public int increment(CommandContext<CommandSourceStack> context) {
-        switch (StringArgumentType.getString(context, "type")) {
-            case "embers" -> {
-                if (embers < 60) {
-                    interpolate(emberPos[0], emberPos[1], embers++, emberIcons, false);
-                } return embers;}
-            case "treasure" -> {
-                if (treasure < 60) {
-                    interpolate(treasurePos[0], treasurePos[1], treasure++, treasureIcons, false);
-                } return treasure;}
-            case "hazard" -> {
-                if (hazard < 60) {
-                    interpolate(hazardPos[0], hazardPos[1], hazard++, hazardIcons, false);
-                } return hazard;}
-            case "clank" -> {
-                if (clank < 60) {
-                    interpolate(clankPos[0], clankPos[1], clank++, clankIcons, false);
-                } return clank;}
-            case "recycles" -> {
-                if (recycles < 3) {
-                    interpolate(recyclePos[0] + recycles * 3, recyclePos[1], 2, 2, recycleIcon);
-                    recycles++;
-                } return recycles;}
-            case "cards" -> {
-                if (cards < 40) {
-                    interpolate(cardPos[0] + (cards / 2) * 3, cardPos[1] + (cards % 2) * 5, 2, 3, cardIcon);
-                    cards ++;
-                } return cards;}
-        }
-        return 0;
-    }
-
-    public int decrement(CommandContext<CommandSourceStack> context) {
-        switch (StringArgumentType.getString(context, "type")) {
-            case "embers" -> {
-                if (embers > 0) {
-                    interpolate(emberPos[0], emberPos[1], --embers, emberIcons, true);
-                } return embers;}
-            case "treasure" -> {
-                if (treasure > 0) {
-                    interpolate(treasurePos[0], treasurePos[1], --treasure, treasureIcons, true);
-                } return treasure;}
-            case "hazard" -> {
-                if (hazard > 0) {
-                    interpolate(hazardPos[0], hazardPos[1], --hazard, hazardIcons, true);
-                } return hazard;}
-            case "clank" -> {
-                if (clank > 0) {
-                    interpolate(clankPos[0], clankPos[1], --clank, clankIcons, true);
-                } return clank;}
-            case "recycles" -> {
-                if (recycles > 0) {
-                    recycles --;
-                    interpolate(recyclePos[0] + recycles * 3, recyclePos[1], 2, 2, fillColour);
-                } return recycles;}
-            case "cards" -> {
-                if (cards > 0) {
-                    cards --;
-                    interpolate(cardPos[0] + (cards / 2) * 3, cardPos[1] + (cards % 2) * 5, 2, 3, fillColour);
-                } return cards;}
-        }
-        return 0;
-    }
-
     private void interpolate(int x, int y, int value, byte[][] icon, boolean decrement) {
         int level = value / 15;
         if (decrement) {
@@ -238,21 +161,112 @@ public class Map {
         }
     }
 
+    public int getValue(CommandContext<CommandSourceStack> context) {
+        int value = switch (StringArgumentType.getString(context, "type")) {
+            case "embers" -> embers;
+            case "treasure" -> treasure;
+            case "hazard_block" -> hazardB;
+            case "clank_block" -> clankB;
+            case "recycles" -> recycles;
+            case "cards" -> cards;
+            case "clank" -> clank;
+            case "hazard" -> hazard;
+
+            default -> 0;
+        };
+        context.getSource().sendSuccess(() -> Component.literal("" + value), false);
+        return value;
+    }
+
+    public int increment(CommandContext<CommandSourceStack> context) {
+        switch (StringArgumentType.getString(context, "type")) {
+            case "embers" -> {
+                if (embers < 60) {
+                    interpolate(emberPos[0], emberPos[1], embers++, emberIcons, false);
+                } return embers;}
+            case "treasure" -> {
+                if (treasure < 60) {
+                    interpolate(treasurePos[0], treasurePos[1], treasure++, treasureIcons, false);
+                } return treasure;}
+            case "hazard_block" -> {
+                if (hazardB < 60) {
+                    interpolate(hazardPos[0], hazardPos[1], hazardB++, hazardIcons, false);
+                } return hazardB;}
+            case "clank_block" -> {
+                if (clankB < 60) {
+                    interpolate(clankPos[0], clankPos[1], clankB++, clankIcons, false);
+                } return clankB;}
+            case "recycles" -> {
+                if (recycles < 3) {
+                    recycles++;
+                    updateRecycle();
+                } return recycles;}
+            case "cards" -> {
+                if (cards < 40) {
+                    interpolate(cardPos[0] + (cards / 2) * 3, cardPos[1] + (cards % 2) * 5, 2, 3, cardIcon);
+                    cards ++;
+                } return cards;}
+            case "clank" -> {if (clankB > 0) {decrement("clank_block");} else {clank++;} return clank;}
+            case "hazard" -> {if (hazardB > 0) {decrement("hazard_block");} else {hazard++;} return hazard;}
+        }
+        return 0;
+    }
+
+    public int decrement(CommandContext<CommandSourceStack> context) {
+        return decrement(StringArgumentType.getString(context, "type"));
+    }
+
+    private int decrement(String type) {
+        switch (type) {
+            case "embers" -> {
+                if (embers > 0) {
+                    interpolate(emberPos[0], emberPos[1], --embers, emberIcons, true);
+                } return embers;}
+            case "treasure" -> {
+                if (treasure > 0) {
+                    interpolate(treasurePos[0], treasurePos[1], --treasure, treasureIcons, true);
+                } return treasure;}
+            case "hazard_block" -> {
+                if (hazardB > 0) {
+                    interpolate(hazardPos[0], hazardPos[1], --hazardB, hazardIcons, true);
+                } return hazardB;}
+            case "clank_block" -> {
+                if (clankB > 0) {
+                    interpolate(clankPos[0], clankPos[1], --clankB, clankIcons, true);
+                } return clankB;}
+            case "recycles" -> {
+                if (recycles > 0) {
+                    recycles --;
+                    updateRecycle();
+                } return recycles;}
+            case "cards" -> {
+                if (cards > 0) {
+                    cards --;
+                    interpolate(cardPos[0] + (cards / 2) * 3, cardPos[1] + (cards % 2) * 5, 2, 3, fillColour);
+                } return cards;}
+            case "clank" -> {clank--; return clank;}
+            case "hazard" -> {hazard--; return hazard;}
+        }
+        return 0;
+    }
+
     public int set(CommandContext<CommandSourceStack> context) {
-        int value = IntegerArgumentType.getInteger(context, "value");
+        byte value = (byte) IntegerArgumentType.getInteger(context, "value");
         switch (StringArgumentType.getString(context, "type")) {
             case "embers" -> { if (value <= 60) {
-                embers = (byte) value; update(emberPos, emberIcons, embers); return 1;} return 0;}
+                embers = value; update(emberPos, emberIcons, embers); return 1;} return 0;}
             case "treasure" -> { if (value <= 60) {
-                treasure = (byte) value; update(treasurePos, treasureIcons, treasure); return 1;} return 0;}
-            case "hazard" -> { if (value <= 60) {
-                hazard = (byte) value; update(hazardPos, hazardIcons, hazard); return 1;} return 0;}
-            case "clank" -> { if (value <= 60) {
-                clank = (byte) value; update(clankPos, clankIcons, clank); return 1;} return 0;}
+                treasure = value; update(treasurePos, treasureIcons, treasure); return 1;} return 0;}
+            case "hazard_block" -> { if (value <= 60) {
+                hazardB = value; update(hazardPos, hazardIcons, hazardB); return 1;} return 0;}
+            case "clank_block" -> { if (value <= 60) {
+                clankB = value; update(clankPos, clankIcons, clankB); return 1;} return 0;}
             case "recycles" -> { if (value <= 3) {
-                recycles = (byte) value; updateRecycle(); return 1;} return 0;}
+                recycles = value; updateRecycle(); return 1;} return 0;}
             case "cards" -> { if (value <= 40) {
-                cards = (byte) value; updateCards(); return 1;} return 0;}
+                cards = value; updateCards(); return 1;} return 0;}
+            case "clank" -> {clank = value; return 1;}
+            case "hazard" -> {hazard = value; return 1;}
             default -> {return 0;}
         }
     }
@@ -278,18 +292,14 @@ public class Map {
             pos[1] += 5;
         }
 
-        pos = recyclePos.clone();
-        for (int j = 0; j < 3; j++) {
-            fill(pos[0], pos[1], 2, 2, fillColour);
-            pos[0] += 3;
-        }
-
         embers = 0;
         treasure = 0;
-        hazard = 0;
-        clank = 0;
+        hazardB = 0;
+        clankB = 0;
         recycles = 0;
         cards = 0;
+
+        updateRecycle();
 
         return 1;
     }
@@ -328,13 +338,13 @@ public class Map {
         int i = 0;
 
         while (i < recycles) {
-            printIcon(pos[0], pos[1], 2, 2, recycleIcon);
-            pos[0] += 3;
+            replaceIn(pos[0], pos[1], 3, 6, (byte) 113, (byte) 132);
+            pos[0] += 5;
             i++;
         }
         while (i < 3) {
-            fill(pos[0], pos[1], 2, 2, fillColour);
-            pos[0] += 3;
+            replaceIn(pos[0], pos[1], 3, 6, (byte) 132, (byte) 113);
+            pos[0] += 5;
             i++;
         }
     };
@@ -354,30 +364,33 @@ public class Map {
     }
 
     public int colourMap(CommandContext<CommandSourceStack> context) {
-        byte colour_new = (byte) IntegerArgumentType.getInteger(context,"new_colour");
-        byte colour     = (byte) IntegerArgumentType.getInteger(context,"colourID");
+        byte colour_new = (byte) IntegerArgumentType.getInteger(context,"colourID");
 
         for (int i = 0; i < mapData.colors.length; i++) {
-            if (mapData.colors[i] == colour) {mapData.setColor(i % 128, i / 128, colour_new);}
+            if (mapData.colors[i] == 0) {mapData.setColor(i % 128, i / 128, colour_new);}
         }
 
         return 1;
     }
-    public int colourRectangle(CommandContext<CommandSourceStack> context) {
-        byte colour_new = (byte) IntegerArgumentType.getInteger(context,"new_colour");
-        byte colour     = (byte) IntegerArgumentType.getInteger(context,"colour");
-        int x = IntegerArgumentType.getInteger(context, "x");
-        int y = IntegerArgumentType.getInteger(context, "y");
-        int width = IntegerArgumentType.getInteger(context, "width");
-        int height = IntegerArgumentType.getInteger(context, "height");
 
+    public int replaceIn(CommandContext<CommandSourceStack> context) {
+        replaceIn(
+          (byte)IntegerArgumentType.getInteger(context, "x"),
+          (byte)IntegerArgumentType.getInteger(context, "y"),
+                IntegerArgumentType.getInteger(context, "width"),
+                IntegerArgumentType.getInteger(context, "height"),
+          (byte)IntegerArgumentType.getInteger(context, "colour"),
+          (byte)IntegerArgumentType.getInteger(context, "new_colour")
+        );
+        return 1;
+    }
+
+    public void replaceIn(byte x, byte y, int width, int height, byte colour, byte colour_new) {
         for (int i = x; i < x + width; i++) {
             for (int j = y; j < y + height; j++) {
                 if (mapData.colors[i + j * 128] == colour) {mapData.setColor(i, j, colour_new);}
             }
         }
-
-        return 1;
     }
 
     public int getColour(CommandContext<CommandSourceStack> context) {
@@ -385,7 +398,27 @@ public class Map {
         int y = IntegerArgumentType.getInteger(context, "y");
         int colour = mapData.colors[x + y * 128];
 
-        context.getSource().sendSuccess(() -> Component.literal("" + colour), false);
+        context.getSource().sendSuccess(() -> Component.literal("" + (colour < 0 ? 256 + colour : colour)), false);
         return colour;
+    }
+
+    public static int copy(CommandContext<CommandSourceStack> context) {
+        ServerLevel level = context.getSource().getLevel();
+        MapItemSavedData to = level.getMapData(new MapId(IntegerArgumentType.getInteger(context, "to")));
+        MapItemSavedData from = level.getMapData(new MapId(IntegerArgumentType.getInteger(context, "ID")));
+
+        try {
+            for (int i = 0; i < 128; i++) {
+                for (int j = 0; j < 128; j++) {
+                    to.setColor(i,j, from.colors[i + j * 128]);
+                }
+            }
+
+        } catch (NullPointerException e) {
+            context.getSource().sendFailure(Component.literal("Map not found in the current dimension"));
+            return 0;
+        }
+
+        return 1;
     }
 }
